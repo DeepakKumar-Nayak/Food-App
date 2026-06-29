21b2eecf868d9187e800c4c1ae246e44063938f8
Deepak Kumar Nayak <deepakkumarnk89@gmail.com>
2026-06-29 22:15:58 +0530

    Fix: removed tracked build files

--- dist/React.6bd02f5a.js
+++
@@ -1,7 +0,0 @@
-const heading = React.createElement('h1', {
-    id: 'heading'
-}, 'Hello React');
-const root = ReactDOM.createRoot(document.querySelector('#root'));
-root.render(heading);
-
-//# sourceMappingURL=React.6bd02f5a.js.map
